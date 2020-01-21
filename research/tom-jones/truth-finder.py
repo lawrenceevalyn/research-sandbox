9e1ca2b8c4ad951647cec0e5f342b86dbc03138b
Lawrence <lawrenceevalyn@gmail.com>
2020-01-21 17:54:57 -0500

    got it working prior to regex
    it'll spit out all lines that include Allworthy

--- research/tom-jones/truth-finder.py
+++ research/tom-jones/truth-finder.py
@@ -1,34 +1,23 @@
 import re	#regular expressons
 
-filepath = Tom-Jones.txt #change as needed
+input = "Tom-Jones.txt" #change as needed
+output = "output.csv"
 
 verbRegex = "to\s(.|..|...|....|.....|......|.......|........|.........|..........|...........|............|.............|..............|...............)\s(truth|the\struth|truly)"
 # look for something that follows one of these patterns:
 	# "to ____ the truth"
 	# "to ____ truth"
 	# "to ____ truly"
+# (this regex needs the word "to", then a space, then 1-15 characters of any kind, then a space, then either "truth", "the truth", or "truly".)
 
-truthRegex = 
-
-def find(regex,source):
-
-# first I want to see all the possible strings I should consider!
+# truthRegex = 
 
 
-	# if a line matches, put it in a new row in a csv
-	# also add the line number to another column in that row
-
-# after I've done the above, I'll write code that does this part:
-# for each line, look for any of my chosen strings:
-	# "to say the truth"
-	# "to say truth"
-	# "to speak the truth"
-	# "to speak truly"
-	# "to speak more truly"
-	# "to confess the truth"
-	# "why should we not confess the truth"
+def find(regex,text):
+	with open(text,"r") as file:
+		for line in file:
+			if "Allworthy" in line:
+				print(line)
+				# add it to the output csv
 	
-	# if a line matches, put it in a new row in a csv
-	# also add the line number to another column in that row
-	
-find (verbRegex,filepath)
+find (verbRegex,input)
